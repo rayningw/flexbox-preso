be87332c288620b28dd970304e7a2bc52d5cd62d
Ray Wu <rayning.wu@gmail.com>
2017-10-29 09:04:09 +1100

    Add eslintrc and make quotes consistent

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"commonjs": true,
+		"node": true,
+		"es6": true,
+		"jest": true
+	},
+	"extends": ["eslint:recommended", "plugin:react/recommended"],
+	"parserOptions": {
+		"ecmaFeatures": {
+			"experimentalObjectRestSpread": true,
+			"jsx": true
+		},
+		"sourceType": "module"
+	},
+	"plugins": [
+		"flowtype",
+		"react"
+	],
+	"rules": {
+		"indent": [
+			"error",
+			2
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"semi": [
+			"error",
+			"always"
+		],
+		"no-mixed-spaces-and-tabs": [
+			"error",
+			"smart-tabs"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"no-console": [
+			"error",
+			{ "allow": ["log", "info", "warn", "error"] }
+		],
+	}
+};
