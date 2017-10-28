1a0eaff7113c7030e7f3205cf0eabf72ed2cc0ab
Ray Wu <rayning.wu@gmail.com>
2017-10-29 09:49:26 +1100

    Rudimentary navigation of screens

--- src/app/app.js
+++ src/app/app.js
@@ -4,19 +4,57 @@ import Screen from "../screen";
 
 import "./app.css";
 
-class App extends Component {
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      /** Linear sequence of screens */
+      screens: [
+        {
+          title: "Screen One",
+          left: "This is the left text",
+          right: "This is the right text",
+        },
+        {
+          title: "Screen Two",
+          left: "This is the left text 2",
+          right: "This is the right text 2",
+        },
+      ],
+      /** Index of the current screen */
+      currentScreen: 0,
+    };
+  }
+
+  handleGoBack() {
+    this.setState({
+      currentScreen: Math.max(0, this.state.currentScreen - 1),
+    });
+  }
+
+  handleGoForward() {
+    this.setState({
+      currentScreen: Math.min(this.state.screens.length - 1, this.state.currentScreen + 1),
+    });
+  }
+
   render() {
+    const screen = this.state.screens[this.state.currentScreen];
+
     return (
       <div className="app">
         <header className="app-header">
-          <h1 className="app-title">Flexbox Preso</h1>
+          <h1 className="app-title">{screen.title}</h1>
+          <div onClick={this.handleGoBack.bind(this)}>Go Back</div>
+          <div onClick={this.handleGoForward.bind(this)}>Go Foward</div>
         </header>
         <div className="app-screens">
-          <Screen left="This is the left text" right="This is the right text" />
+          <Screen left={screen.left} right={screen.right} />
         </div>
       </div>
     );
   }
-}
 
-export default App;
+}
